33564f1b03fef72701a0523d577327e893fdf45a
wang.ning <wang.ning_1920@immomo.com>
2019-09-16 14:09:43 +0800

    初始化网络处理类

---
+++ ADAPIBaseManager/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ADAPIBaseManager
+//
+//  Created by David on 2019/9/16.
+//  Copyright © 2019年 ADIOS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ADAPIBaseManager/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  ADAPIBaseManager
+//
+//  Created by David on 2019/9/16.
+//  Copyright © 2019年 ADIOS. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
